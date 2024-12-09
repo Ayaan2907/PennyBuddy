bd4ad8afcadaa1624b05db7783142fb7428a9fea
Ayaan Kaifullah <ayaanansari7921@outlook.com>
2024-12-09 14:56:22 -0500

    typo

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://financial-management-system-backend-na41pkt2e.vercel.app/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://financial-management-system-backend-na41pkt2e.vercel.app";
 
 // temp fix, hardcoding BE deployment url
 
