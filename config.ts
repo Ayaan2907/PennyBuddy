2c71c4ccdf44e08b27a8be147ba113bc77777da4
Ayaan Kaifullah <ayaanansari7921@outlook.com>
2024-12-09 14:54:01 -0500

    temp fix: put BE url in config

--- config.ts
+++ config.ts
@@ -1,4 +1,6 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3003";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://financial-management-system-backend-na41pkt2e.vercel.app/";
+
+// temp fix, hardcoding BE deployment url
 
 export default {
   API_URL,
